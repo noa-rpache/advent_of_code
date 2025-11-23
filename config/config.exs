ec6c7c1c779991f3318b06d4e3cc37b31049bdd0
noa-sb <noa.rpache@udc.es>
2025-11-23 23:41:58 +0100

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+import Config
+
+config :aoc,
+  # The prefix is used when creating solutions and test modules with
+  # `mix aoc.create`.
+  prefix: AdventOfCode.Solutions,
+
+  # Optional
+
+  # Include help comments when generating modules and tests.
+  generate_comments: true
